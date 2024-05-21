d8742a8a319deba3f12cba70897f5fbd7fd10bbb
dheenadayalann33 <dheenadayalan33@gmail.com>
2024-05-21 07:26:52 +0530

    first init

---
+++ src/Components/Images.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+function Images({ element }) {
+    return (
+        <>
+            <Carousel>
+                <Carousel.Item>
+                    <img src={element.images[0]} class="d-block w-100" alt="..." style={{height:'300px'}}/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={element.images[1]} class="d-block w-100" alt="..." style={{height:'300px'}}/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={element.images[2]} class="d-block w-100" alt="..." style={{height:'300px'}}/> 
+                </Carousel.Item>
+            </Carousel>
+        </>
+    );
+}
+
+export default Images;
